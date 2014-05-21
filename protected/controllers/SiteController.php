0d1b8065bb7e9c603349c7627800f7773b061e21
Mark Wadham <mark.wadham@openeyes.org.uk>
2014-05-21 12:42:00 +0000

    hacky bugfix

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -209,6 +209,10 @@ class SiteController extends BaseController
 			$institution = Institution::model()->find('source_id=? and remote_id=?',array(1,'RP6'));
 		}
 
+		if (!$institution) {
+			$institution = Institution::model()->find();
+		}
+
 		$criteria = new CDbCriteria;
 		$criteria->compare('institution_id',$institution->id);
 		$criteria->order = 'short_name asc';
