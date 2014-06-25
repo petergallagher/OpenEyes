77a1dc1cc618bc0ca80a6ddf0497ebc5b25d15eb
Mark Wadham <mark.wadham@openeyes.org.uk>
2014-06-25 14:03:10 +0000

    bugfix

--- protected/services/PatientService.php
+++ protected/services/PatientService.php
@@ -52,7 +52,7 @@ class PatientService extends DeclarativeModelService
 		'PatientAddress' => array(
 			'ar_class' => 'Address',
 			'related_objects' => array(
-				'contact' => array('contact_id', 'Contact'),
+				'contact' => array('contact_id', 'Contact', 'save' => 'no'),
 			),
 			'reference_objects' => array(
 				'country' => array('country_id', 'Country', array('name')),
