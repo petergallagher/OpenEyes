f4137a3e25f1cd8646e4aa6e78228fd103c9c4e1
adamdyer <progen91@gmail.com>
2014-07-29 16:26:03 +0100

    [OE-4425] rbac migration fix

--- protected/migrations/m140521_150455_patient_history_rbac.php
+++ protected/migrations/m140521_150455_patient_history_rbac.php
@@ -5,7 +5,7 @@ class m140521_150455_patient_history_rbac extends CDbMigration
 	public function up()
 	{
 		$this->insert('authitem',array('name'=>'OprnEditSocialHistory', 'type' => 0));
-		$this->insert('authitemchild',array('parent'=>'TaskEditPatientData','child'=>'OprnEditSocialHistory', 'type' => 1));
+		$this->insert('authitemchild',array('parent'=>'TaskEditPatientData','child'=>'OprnEditSocialHistory'));
 	}
 
 	public function down()
