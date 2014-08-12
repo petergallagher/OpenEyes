c8d3957fc52d5313feb5dd8427155607a68f50b6
adamdyer <progen91@gmail.com>
2014-08-12 15:30:41 +0100

    [OE-4625] event relations

--- protected/models/Event.php
+++ protected/models/Event.php
@@ -106,7 +106,8 @@ class Event extends BaseActiveRecordVersioned
 			'usermodified' => array(self::BELONGS_TO, 'User', 'last_modified_user_id'),
 			'eventType' => array(self::BELONGS_TO, 'EventType', 'event_type_id'),
 			'issues' => array(self::HAS_MANY, 'EventIssue', 'event_id'),
-			'children' => array(self::HAS_MANY, 'EventChildAssignment', 'parent_event_id'),
+			'parent' => array(self::HAS_ONE, 'EventRelation', 'child_event_id'),
+			'children' => array(self::HAS_MANY, 'EventRelation', 'parent_event_id'),
 		);
 	}
 
