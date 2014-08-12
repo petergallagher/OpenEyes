9e47178aa6e142e33ebcc92792ad2946299967e9
adamdyer <progen91@gmail.com>
2014-08-12 15:02:47 +0100

    [OE-4625] event model update

--- protected/models/Event.php
+++ protected/models/Event.php
@@ -106,8 +106,7 @@ class Event extends BaseActiveRecordVersioned
 			'usermodified' => array(self::BELONGS_TO, 'User', 'last_modified_user_id'),
 			'eventType' => array(self::BELONGS_TO, 'EventType', 'event_type_id'),
 			'issues' => array(self::HAS_MANY, 'EventIssue', 'event_id'),
-			'parent' => array(self::BELONGS_TO, 'Event', 'parent_id'),
-			'children' => array(self::HAS_MANY, 'Event', 'parent_id'),
+			'children' => array(self::HAS_MANY, 'EventChildAssignment', 'parent_event_id'),
 		);
 	}
 
