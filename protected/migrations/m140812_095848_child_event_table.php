3c9b07b0fbf84ec22aa92be9f54273974214ac24
adamdyer <progen91@gmail.com>
2014-08-12 11:11:38 +0100

    [OE-4625] child event mapping table

---
+++ protected/migrations/m140812_095848_child_event_table.php
@@ -0,0 +1,25 @@
+<?php
+
+class m140812_095848_child_event_table extends OEMigration
+{
+	public function up()
+	{
+		$this->createOETable(
+			"child_event",
+			array(
+				"id" => "pk",
+				"parent_event_id" => "INT(10) UNSIGNED NOT NULL",
+				"child_event_id" => "INT(10) UNSIGNED NOT NULL",
+				'constraint child_event_parent_event_id_fk foreign key (parent_event_id) references event (id)',
+				'constraint child_event_child_event_id_fk foreign key (child_event_id) references event (id)',
+			),
+			true
+		);
+	}
+
+	public function down()
+	{
+		$this->dropTable("child_event");
+	}
+
+}
